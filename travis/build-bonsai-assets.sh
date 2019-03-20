9046cee6a05a43b33cc050ed61b5bb9629d0055b
Jef Spaleta <jef@sensu.io>
2019-03-20 01:27:13 -0800

    adjust bonsai file naming

--- travis/build-bonsai-assets.sh
+++ travis/build-bonsai-assets.sh
@@ -18,15 +18,15 @@ GIT_REF=${TRAVIS_COMMIT}
 if [ -d dist ]; then
   # Build Debian asset
   docker build --build-arg "ASSET_GEM=${GEM_NAME}" --build-arg "GIT_REPO=${GIT_REPO}"  --build-arg "GIT_REF=${GIT_REF}" -t ruby-plugin-debian -f Dockerfile.debian .
-  docker cp $(docker create --rm ruby-plugin-debian:latest sleep 0):/${GEM_NAME}.tar.gz ./dist/${GEM_NAME}_${TAG}_debian_linux_amd64.tar.gz
+  docker cp $(docker create --rm ruby-plugin-debian:latest sleep 0):/${GEM_NAME}.tar.gz ./dist/debian_linux_amd64.tar.gz
 
   # Build Alpine asset
   docker build --build-arg "ASSET_GEM=${GEM_NAME}" --build-arg "GIT_REPO=${GIT_REPO}"  --build-arg "GIT_REF=${GIT_REF}" -t ruby-plugin-alpine:latest -f Dockerfile.alpine .
-  docker cp $(docker create --rm ruby-plugin-alpine:latest sleep 0):/${GEM_NAME}.tar.gz ./dist/${GEM_NAME}_${TAG}_alpine_linux_amd64.tar.gz
+  docker cp $(docker create --rm ruby-plugin-alpine:latest sleep 0):/${GEM_NAME}.tar.gz ./dist/alpine_linux_amd64.tar.gz
 
   # Build CentOS asset
   docker build --build-arg "ASSET_GEM=${GEM_NAME}" --build-arg "GIT_REPO=${GIT_REPO}"  --build-arg "GIT_REF=${GIT_REF}" -t ruby-plugin-centos:latest -f Dockerfile.centos .
-  docker cp $(docker create --rm ruby-plugin-centos:latest sleep 0):/${GEM_NAME}.tar.gz ./dist/${GEM_NAME}_${TAG}_centos_linux_amd64.tar.gz
+  docker cp $(docker create --rm ruby-plugin-centos:latest sleep 0):/${GEM_NAME}.tar.gz ./dist/centos_linux_amd64.tar.gz
 
   # Generate the sha512sum for all the assets
   files=$( ls dist/*.tar.gz )
@@ -43,7 +43,7 @@ if [ -d dist ]; then
   if [ -n "$package" ]; then
     echo "Generating sha512sum for ${package}"
     cd dist || exit
-    sha512_file="${package}_sha512-checksums.txt"
+    sha512_file="sha512-checksums.txt"
     #echo "${sha512_file}" > sha512_file
     echo "sha512_file: ${sha512_file}"
     sha512sum ./*.tar.gz > "${sha512_file}"
